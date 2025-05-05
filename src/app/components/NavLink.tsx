3f1b7679a8a289e3c46e06cf9e46d65f33d32af7
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-05-06 02:45:21 +0300

    +

--- src/app/components/NavLink.tsx
+++ src/app/components/NavLink.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 export interface NavLinkPropTypes {
     name: string;
     path: string;
-    icon?: string;
+    icon: string;
   }
 
 const NavLink = ({ name, path, icon }: NavLinkPropTypes) => {
